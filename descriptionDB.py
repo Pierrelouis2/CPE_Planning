96cf469694cb3381e878e6dd051e8abc28e05dfa
jo-pouradier <jocharbo43@gmail.com>
2023-03-17 01:14:05 +0100

    tkt

--- descriptionDB.py
+++ descriptionDB.py
@@ -3,5 +3,5 @@ import sqlite3
 from sqlite3 import Error
 import pandas as pd
 con = sqlite3.connect("users.db")
-df = pd.read_sql_query("SELECT * from users", con)
+df = pd.read_sql_query("SELECT * from user", con)
 print(df)
