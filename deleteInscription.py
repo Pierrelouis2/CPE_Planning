32e5816017e537f2107d88f79527bd739590fb80
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-03-26 22:34:40 +0200

    ok

--- deleteInscription.py
+++ deleteInscription.py
@@ -1,15 +1,16 @@
 import sqlite3
 
 conn = sqlite3.connect('users.db')
+c = conn.cursor()
 
 init = input("delete inscription 1 ou user 2 : ")
 if init == "1":
-    sql_delete_iscriptions = "DELETE FROM user where status = inscription"
-    conn.execute(sql_delete_iscriptions)
+    sql_delete_iscriptions = 'DELETE FROM user where status="Inscription"'
+    c.execute(sql_delete_iscriptions)
     print("Inscriptions supprimées")
 elif init == "2":
     user_id = input("Id user a supprimer : ")
     sql_delete_user = "DELETE FROM user WHERE id_user=?"
-    conn.execute(sql_delete_user, (user_id))
+    c.execute(sql_delete_user, (user_id))
     print("User supprimé")
 
