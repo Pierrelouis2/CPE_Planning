da76ab05483185e1624134a9482fdbdb5c3d6f2f
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-03-27 11:42:02 +0200

    path

--- outils_db/descriptionDB.py
+++ outils_db/descriptionDB.py
@@ -2,6 +2,6 @@
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
-con = sqlite3.connect("users.db")
+con = sqlite3.connect("../users.db")
 df = pd.read_sql_query("SELECT * from user", con)
 print(df)
