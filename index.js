1e2103aba8bbb5759bd521039969afc026177c87
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-02 16:54:25 +0200

    no return

--- index.js
+++ index.js
@@ -191,6 +191,7 @@ function set_get_started() {
   } else {
     console.error("Unable to send message:" + err);
   }
+  return;
 }
 
 // Set up the persistent menu
@@ -210,17 +211,16 @@ async function set_persistent_menu(psid) {
   } else {
     console.error("Unable to send message:" + err);
   }
-  return None;
+  return;
 }
 
 // Handling the message when a user send text and not a postback
 async function handleMessage(sender_psid) {
   let response = templates.askTemplateJour();
   let r;
-  let result = await set_persistent_menu(sender_psid);
+  await set_persistent_menu(sender_psid);
   r = await callSendAPI(sender_psid, response[0]);
   r = await callSendAPI(sender_psid, response[1]);
-  return None;
 }
 
 // Handling the message when a user send a postback
@@ -229,7 +229,7 @@ async function handlePostback(sender_psid, received_postback) {
   let message;
   let r;
   let sql_set_filiere;
-  let result = await set_persistent_menu(sender_psid); // Needed here ?
+  await set_persistent_menu(sender_psid); // Needed here ?
   // Get the payload for the postback
   let payload = received_postback.payload;
   console.log("payload: ", payload);
