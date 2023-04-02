13479a6f34fc26dd8ae32a6e0564c57339646f9a
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-02 16:46:10 +0200

    require

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ let express = require("express"),
   sqlite3 = require("sqlite3"),
   fs = require("fs"),
   { promisify } = require("util"),
+  templates = require("./templates"),
   Date = require("date-and-time");
   
-import { templates } from "./templates.js";
 
 templates.test();
 // INIT APP
