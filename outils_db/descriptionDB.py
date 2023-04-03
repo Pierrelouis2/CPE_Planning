1fed3d8bca0303dd847252fa90a229f280d3e271
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-03 19:55:47 +0200

    desc input

--- outils_db/descriptionDB.py
+++ outils_db/descriptionDB.py
@@ -6,18 +6,30 @@ conn = sqlite3.connect("users.db")
 
 choix = input("quelle table (user : 1  liaison : 2  mso : 3) ?")
 
-df = pd.read_sql_query("SELECT * from user", conn)
-print(df.to_string())
+if choix == "1":
+    df = pd.read_sql_query("SELECT * from user", conn)
+    print(df.to_string())
+elif choix == "2":
+    df = pd.read_sql_query("SELECT * FROM tj_user_mso", conn)
+    print(df.to_string())
+elif choix == "3":
+    df = pd.read_sql_query("SELECT * from mso", conn)
+    print(df.to_string())
+else:
+    print("erreur")
+
+# df = pd.read_sql_query("SELECT * from user", conn)
+# print(df.to_string())
+# # df = pd.read_sql_query("SELECT * from mso", conn)
+# # print(df.to_string())
+# # df = pd.read_sql_query("SELECT * FROM tj_user_mso", conn)
+# # df = pd.read_sql_query("SELECT * from user", conn)
+# # print(df.to_string())
 # df = pd.read_sql_query("SELECT * from mso", conn)
 # print(df.to_string())
 # df = pd.read_sql_query("SELECT * FROM tj_user_mso", conn)
-# df = pd.read_sql_query("SELECT * from user", conn)
 # print(df.to_string())
-df = pd.read_sql_query("SELECT * from mso", conn)
-print(df.to_string())
-df = pd.read_sql_query("SELECT * FROM tj_user_mso", conn)
-print(df.to_string())
 
-# id = 111
-# df = pd.read_sql_query("SELECT mso.name_mso FROM mso INNER JOIN tj_user_mso ON tj_user_mso.id_mso = mso.id_mso WHERE tj_user_mso.id_user=? ", conn, params=(id, ))
-# print(df.to_string())
+# # id = 111
+# # df = pd.read_sql_query("SELECT mso.name_mso FROM mso INNER JOIN tj_user_mso ON tj_user_mso.id_mso = mso.id_mso WHERE tj_user_mso.id_user=? ", conn, params=(id, ))
+# # print(df.to_string())
