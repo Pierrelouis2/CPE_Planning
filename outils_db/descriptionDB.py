3c09a337f33697c1db4047f1df3b0a9d7be303fc
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-03 19:54:44 +0200

    desc

--- outils_db/descriptionDB.py
+++ outils_db/descriptionDB.py
@@ -3,6 +3,9 @@ import sqlite3
 from sqlite3 import Error
 import pandas as pd
 conn = sqlite3.connect("users.db")
+
+choix = input("quelle table (user : 1  liaison : 2  mso : 3) ?")
+
 df = pd.read_sql_query("SELECT * from user", conn)
 print(df.to_string())
 # df = pd.read_sql_query("SELECT * from mso", conn)
