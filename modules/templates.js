061d3968f8c91a6e6a289b18c958b8a6150e798a
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-03 10:03:04 +0200

    ok

--- modules/templates.js
+++ modules/templates.js
@@ -234,25 +234,25 @@ function askTemplateMenu(psid) {
 
 function askTemplateMsoCGP() {
     const MSO = {
-    "SSO": "Stratégie de synthèse organique",
-    "CO2": "Chimie Organometallique 2, approche orbitalaire",
-    "IM": "Ingénierie Macromoléculaire",
-    "SSP": "Simulation stationnaire des procédés",
-    "CMH": "Chimie médicinale et hétérocycles",
-    "GRCA": "Génie de la réaction chimique avancée",
-    "TE": "Transition énergétique",
-    "AL": "Analyses en lignes",
-    "SM": "Synthèse Macromoléculaire",
-    "SMB": "Synthèse de molécules bioactives",
-    "NN": "Nanochimie, nanomatériaux",
-    "CN": "Chimie nucléaire",
-    "ADNSC": "Analyse de données - le numérique au service de la chimie",
-    "CAM": "Conception et application du médicament",
-    "TSA": "Techniques séparatives avancées",
-    "CDD": "Catalyse et développement durable",
-    "GP": "Génie de la polymérisation",
-    "RMN": "RMN appliquée à la chimie moléculaire",
-    "MN": "Méthodes Numériques"
+        SSO: "Stratégie de synthèse organique",
+        CO2: "Chimie Organometallique 2, approche orbitalaire",
+        IM: "Ingénierie Macromoléculaire",
+        SSP: "Simulation stationnaire des procédés",
+        CMH: "Chimie médicinale et hétérocycles",
+        GRCA: "Génie de la réaction chimique avancée",
+        TE: "Transition énergétique",
+        AL: "Analyses en lignes",
+        SM: "Synthèse Macromoléculaire",
+        SMB: "Synthèse de molécules bioactives",
+        NN: "Nanochimie, nanomatériaux",
+        CN: "Chimie nucléaire",
+        ADNSC: "Analyse de données - le numérique au service de la chimie",
+        CAM: "Conception et application du médicament",
+        TSA: "Techniques séparatives avancées",
+        CDD: "Catalyse et développement durable",
+        GP: "Génie de la polymérisation",
+        RMN: "RMN appliquée à la chimie moléculaire",
+        MN: "Méthodes Numériques"
     }
     let i = 0;
     let lst_message = [];
