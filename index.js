9f87e18297f574130b639cc1912a074bf75eeaa1
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-05 23:27:32 +0200

    test

--- index.js
+++ index.js
@@ -56,23 +56,23 @@ const MAJEURS = {
   IMI: "IMAGE",
 };
 const MSO = {
-    SSO: "Stratégie de Synthèse Organique",
-    IM: "Ingéniérie Macromoléculaire",
-    SMA: "Spectrométries RMN et Masse Avancées",
-    IBB: "Introduction aux Biotechnologies et Bioprocédés",
-    SP: "Simulation des Procédés",
-    COO: "Chimie Organométallique et Approche Orbitalaire",
-    CAM: "Conception et Application du Médicament",
-    TSS: "Techniques Séparatives avancées et Spéciation",
-    CDD: "Catalyse et Développement Durable",
-    GP: "Génie de la Polymérisation",
-    MSSO: "Méthodes Spectroscopiques pour la Synthèse Organique",
-    MN: "Méthodes Numériques",
-    SMB: "Synthèse de molécules bioactives",
-    MN: "De la molécule aux nanomatériaux",
-    CNMACC: "Chimie nucléaire, mesure, analyse et cycle du combustible",
-    CN: "Chimie et Numérique",
-    MIE: "Microbiologie, Immunologie, Eléments de génie génétique"
+  "SSO": "Stratégie de Synthèse Organique",
+  "IM": "Ingéniérie Macromoléculaire",
+  "SMA": "Spectrométries RMN et Masse Avancées",
+  "IBB": "Introduction aux Biotechnologies et Bioprocédés",
+  "SP": "Simulation des Procédés",
+  "COO": "Chimie Organométallique et Approche Orbitalaire",
+  "CAM": "Conception et Application du Médicament",
+  "TSS": "Techniques Séparatives avancées et Spéciation",
+  "CDD": "Catalyse et Développement Durable",
+  "GP": "Génie de la Polymérisation",
+  "MSSO": "Méthodes Spectroscopiques pour la Synthèse Organique",
+  "MN": "Méthodes Numériques",
+  "SMB": "Synthèse de molécules bioactives",
+  "MN": "De la molécule aux nanomatériaux",
+  "CNMACC": "Chimie nucléaire, mesure, analyse et cycle du combustible",
+  "CN": "Chimie et Numérique",
+  "MIE": "Microbiologie, Immunologie, Eléments de génie génétique"
 };
 
 // TO CHANGE PASSWORD AND USERNAME TEST
