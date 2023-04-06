3b29c9cfb3a511670dbe42265b114a1c5b438b5b
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-06 16:42:38 +0200

    clean

--- static/public/js/home.js
+++ static/public/js/home.js
@@ -3,23 +3,7 @@
 // let db = new sqlite3.Database("../../../users.db");
 // const queryDB = promisify(db.all).bind(db);
 
-$(document).ready( function() {
-  
-    $('body').on("click", ".larg div h3", function(){
-      if ($(this).children('span').hasClass('close')) {
-        $(this).children('span').removeClass('close');
-      }
-      else {
-        $(this).children('span').addClass('close');
-      }
-      $(this).parent().children('p').slideToggle(250);
-    });
-    
-    $('body').on("click", "nav ul li a", function(){
-      var title = $(this).data('title');
-      $('.title').children('h2').html(title);
-    });
-  });
+
 
 // sql_get_promo = "SELECT promo FROM user";
 // let promos = (await queryDB(sql_get_promo));
