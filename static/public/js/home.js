d5cdf0faaef22c218cb7331bf0cea839ef97c550
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-06 16:35:32 +0200

    test pie

--- static/public/js/home.js
+++ static/public/js/home.js
@@ -24,20 +24,31 @@ $(document).ready( function() {
 sql_get_promo = "SELECT promo FROM user";
 let promos = (await queryDB(sql_get_promo));
 console.log(promos);
-// var xValues = 
-// new Chart("myChart", {
-//     type: "pie",
-//     data: {
-//       labels: xValues,
-//       datasets: [{
-//         backgroundColor: barColors,
-//         data: yValues
-//       }]
-//     },
-//     options: {
-//       title: {
-//         display: true,
-//         text: "World Wide Wine Production 2018"
-//       }
-//     }
-//   });
+
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+var xValues = 
+new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
