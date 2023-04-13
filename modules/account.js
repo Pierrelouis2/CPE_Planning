9b1495424ce90644c4c66b64351166fa87f40cf9
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-13 15:21:35 +0200

    stop logs

--- modules/account.js
+++ modules/account.js
@@ -1,71 +1,63 @@
-let sqlite3 = require("sqlite3"),
-    { promisify } = require("util"),
-    db = new sqlite3.Database("users.db"),
-    bcrypt = require('bcrypt');
-
-const queryDB = promisify(db.all).bind(db);
-
+const sqlite3 = require("sqlite3"),
+   { promisify } = require("util"),
+   db = new sqlite3.Database("users.db"),
+   bcrypt = require('bcrypt'),
+   queryDB = promisify(db.all).bind(db);
 
 // hash password
 async function hashPassword(plaintextPassword) {
-    const hash = await bcrypt.hash(plaintextPassword, "$2b$10$ZtQNnT5Vqmijvf8R9Sxhee");
-
-    return hash;
+   const hash = await bcrypt.hash(plaintextPassword, "$2b$10$ZtQNnT5Vqmijvf8R9Sxhee");
+   return hash;
 }
 
 // compare password
 async function comparePassword(plaintextPassword, user) {
-    console.log("user : " ,user);
-    sql_get_hash = "SELECT password FROM profile WHERE email=?"
-    let hash = (await queryDB(sql_get_hash,user))[0];
-    if (hash!==undefined){
-        hash = hash.password;
-    }else{
-        hash = "";  // What do we do for this case?
-    }
-    console.log("hash : " ,hash);
-    const result = await bcrypt.compare(plaintextPassword, hash);
-    return result;
+   sql_get_hash = `SELECT password FROM profile WHERE email=?`;
+   let hash = (await queryDB(sql_get_hash,user))[0];
+   if (hash!==undefined){
+      hash = hash.password;
+   }else{
+      hash = "";  // What do we do for this case?
+   }
+   const result = await bcrypt.compare(plaintextPassword, hash);
+   return result;
 }
 
 async function changeInfo(info) {
-    let sql_change_info = `UPDATE profile SET prenom=?, nom=?, email=?, password=?, rights=? WHERE psid=?`;
-    await db.run(sql_change_info, [info.prenom, info.nom, info.email, info.password, 'F' ,info.code], function (err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log(`Row(s) updated: ${this.changes}`);
-    });
+   let sql_change_info = `UPDATE profile SET prenom=?, nom=?, email=?, password=?, rights=? WHERE psid=?`;
+   await db.run(sql_change_info, [info.prenom, info.nom, info.email, info.password, 'F' ,info.code], function (err) {
+      if (err) {
+         return console.error(err.message);
+      }
+      console.log(`Row(s) updated: ${this.changes}`);
+   });
 }
 
 async function getProfile(mail) {
-    let sql_get_profile = `SELECT * FROM profile WHERE email=?`;
-    let profile = (await queryDB(sql_get_profile,mail))[0];
-    console.log(profile);
-    return profile;
+   let sql_get_profile = `SELECT * FROM profile WHERE email=?`;
+   let profile = (await queryDB(sql_get_profile,mail))[0];
+   return profile;
 }
 
 async function register(user) {
-    let sql_verify_user = "SELECT * FROM user WHERE id_user=?"
-    let verify = (await queryDB(sql_verify_user,user.psid))[0];
-    if (verify!==undefined){
-        let sql_register = `INSERT INTO profile(psid, prenom, nom, email, password, rights) VALUES(?,?,?,?,?,?)`;
-        await db.run(sql_register, [user.psid, user.prenom, user.nom, user.email, user.password, 'F'], function (err) {
-            if (err) {
-                console.log(err);
-                return 0;
-            } else {
-                console.log(`Row(s) inserted: ${this.changes}`);
-                return 1;
-            }
-        });
-        return 1;
-        }   else {
-            return 0;  
-    }
+   let sql_verify_user = "SELECT * FROM user WHERE id_user=?"
+   let verify = (await queryDB(sql_verify_user,user.psid))[0];
+   if (verify!==undefined){
+      let sql_register = `INSERT INTO profile(psid, prenom, nom, email, password, rights) VALUES(?,?,?,?,?,?)`;
+      await db.run(sql_register, [user.psid, user.prenom, user.nom, user.email, user.password, 'F'], function (err) {
+         if (err) {
+            console.log(err);
+            return 0;
+         } else {
+            console.log(`Account created for ${user.psid}`);
+            return 1;
+         }
+      });
+      return 1;
+   } else {
+      return 0;  
+   }
 }
-
-
 
 module.exports = {
     hashPassword,
