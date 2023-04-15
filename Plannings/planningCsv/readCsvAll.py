c0495eb5c49faa16c10971bc730f22d10467d8df
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-15 22:01:52 +0200

    comments

--- Plannings/planningCsv/readCsvAll.py
+++ Plannings/planningCsv/readCsvAll.py
@@ -2,17 +2,25 @@
 
 import multiprocessing
 import os
+import sys
+
+## use this script from the root of the project : CPE_Planning
 
 path = 'Plannings/planningCsv/'
-# create a script using 4 fork ti run the scripts 4ETI/ReadCsv4ETI.py and 4CGP/ReadCsv4CGP.py and 3ETI/ReadCsv3ETI.py and 3CGP/ReadCsv3CGP.py
-# the script will be run in parallel
+scripts = [path + '4ETI/ReadCsv4ETI.py', path + '4CGP/ReadCsv4CGP.py', path + '3CGP/ReadCsv3CGP.py' , path + '3ETI/ReadCsv3ETI.py']
 
 def run_script(script):
     os.system('python3 ' + script)
 
+def not_main():
+    date = sys.argv[1]
+    for i in range(len(scripts)):
+        scripts[i] += ' ' + date
+    pool = multiprocessing.Pool(processes=4)
+    pool.map(run_script, scripts)
+
 if __name__ == '__main__':
     date = input("Entrez la date du planning csv (ex: 01_04) : ")
-    scripts = [path + '4ETI/ReadCsv4ETI.py', path + '4CGP/ReadCsv4CGP.py', path + '3CGP/ReadCsv3CGP.py'] # , path + '3ETI/ReadCsv3ETI.py'
     # give the date to the scripts as argument
     for i in range(len(scripts)):
         scripts[i] += ' ' + date
