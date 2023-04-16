d56673b92561ee8fcbfcb8f80cb8c0117b7245be
Pierrelouis2 <pierre-louis.telep@cpe.fr>
2023-04-16 20:20:56 +0200

    ok

--- modules/writeMessage.js
+++ modules/writeMessage.js
@@ -157,11 +157,11 @@ async function sendPlanningWeek() {
   user_table.forEach( async (user) => {
     if(user.id_user == 5810016312430121 || user.id_user == 6271457816218293) {
       let message = { text: "Il est Dimache 20h ! Voici le planning de la semaine: " };
-      r = await writeMessage.callSendAPI(user.id_user, message);
+      r = await callSendAPI(user.id_user, message);
       response = templates.askTemplateImage();
       let imgName = user.promo + user.filliere + variables.constant.DATE;
       response.attachment.payload.url = `/png/${imgName}.png`;
-      r = await writeMessage.callSendAPI(user.id_user, response);
+      r = await callSendAPI(user.id_user, response);
   }
   });
 }
