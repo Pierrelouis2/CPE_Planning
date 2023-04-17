b569782675ed32d6e346de28beb806c56b8c8ecb
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2023-04-17 22:44:50 +0200

    try some change

--- Plannings/planningXls/xls2csv.py
+++ Plannings/planningXls/xls2csv.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import dataframe_image as dfi
 import sys
 import os
 
@@ -7,10 +6,9 @@ print('start python script xls2csv.py')
 df = pd.read_excel(sys.argv[1])
 if 'CGP' in sys.argv[1]:
     df = df.drop(df.columns[0], axis=1)
-    df = df.drop(df.index[0:6])
+    df = df.drop(df.index[0:5])
 if 'ETI' in sys.argv[1]:
     pass
 
-dfi.export(df, f'{sys.argv[3]}')
 df.to_csv(f'{sys.argv[2]}', index=False, header=False, sep=';')
 print('end')
