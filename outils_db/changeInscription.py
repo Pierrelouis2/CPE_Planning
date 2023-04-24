dcc3586bcbe86a77659ed8c43c98b525bb0ae879
Pierre-louis <pierre-louis.telep@cpe.fr>
2023-04-24 15:24:39 +0200

    in

--- outils_db/changeInscription.py
+++ outils_db/changeInscription.py
@@ -5,7 +5,7 @@ import sqlite3
 conn = sqlite3.connect('users.db')
 cur = conn.cursor()
 
-init = input("delete all inscription status 1 \n delete user 2 \nchange from inscription to inscrit 3 \n delete profile database 4 \n change rigths pl jo 5 \n :")
+init = input("delete all inscription status 1 \n delete user 2 \nchange from inscription to inscrit 3 \n delete profile database 4 \n change rigths pl jo 5 \n  inscription secreataires 6 :")
 if init == "1":
     sql_delete_iscriptions = 'DELETE FROM user where status = "Inscription"'
     cur.execute(sql_delete_iscriptions)
@@ -33,6 +33,11 @@ elif init == "5":
     cur.execute(sql_change_pl_rights)
     conn.commit()
     print("Inscription changé en inscrit")
+elif init == "6" :
+    sql_set_sec = "INSERT INTO user VALUES (11001,3,'ETI','S',None,'Inscrit'), (11002,3,'CGP','S',None,'Inscrit'),(11001,4,'ETI','S',None,'Inscrit'),(11001,4,'CGP','S',None,'Inscrit')"
+    cur.execute(sql_set_sec)
+    conn.commit()
+    print("Secretaire added")
 else:
     print("erreur")
 
